4834137573c1829263d3f709b264402517920456
LOOPPO <denis.sprinceana@stud.unifi.it>
2018-05-24 00:04:43 +0200

    after a biit

--- iniHandler.cpp
+++ iniHandler.cpp
@@ -7,6 +7,33 @@
 iniHandler::iniHandler(string filename) {
     this->filename=filename;
     this->error=false;
+    this->file.open(filename); //open file
+    string linenow,sectionnow,key,value;
 
-    
+    if(this->file.is_open()){
+....................................................
+    }
+}
+void iniHandler::Close() {
+    if(filename.size()==0)
+        return;
+    .................................
+}
+bool iniHandler::error() {
+    return this->error;
+}
+void iniHandler::set(string section, string key, string value) {
+    container[section][key]=value;
+}
+void iniHandler::setInt(string section, string key, unsigned long long value) {
+    this->set(section,key,to_string(value));
+}
+void iniHandler::setDouble(string section, string key, double value) {
+    this->set(section,key,to_string(value));
+}
+void iniHandler::setBool(string section, string key, bool value) {
+    if(value){
+        this->set(section,key,"TRUE");
+    } else
+        this->set(section,key,"FALSE");
 }
--- iniHandler.h
+++ iniHandler.h
@@ -4,28 +4,38 @@
 
 #ifndef INIPROJECT_INIHANDLER_H
 #define INIPROJECT_INIHANDLER_H
-
 #include <fstream>
 #include <string>
 #include <set>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 class iniHandler {
 public:
     iniHandler(string filename);//constructor
     ~iniHandler();//destructor
-
     bool error();//if an error occur return true
-
-
+    //functions to set values
+    void set(string section,string key,string value);
+    void setInt(string section,string key, unsigned long long value);
+    void setDouble(string section,string key,double value);
+    void setBool(string section,string key,bool value);
+    //functions to read values
+    unsigned long long getInt(string section,string key);
+    double getDouble(string section,string key);
+    bool getBool(string setion,string key);
+    string getString(string section,string key);
+    //functions to delete
+    bool delKey(string section,string key);
+    bool delSection(string section);
+    //something to add news to the file
+    void Close();
 
 private:
     string filename;
-    map<string,map<string,string>>container;
-
+    fstream file;
+    unordered_map<string,unordered_map<string,string>>container;
     bool error;//errors are possible
-
 };
 
 
